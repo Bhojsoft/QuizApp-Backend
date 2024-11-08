aff131ee034f1447f1dfbb38fc76fcf683ff2af4
Mahesh <maheshzalte2000@gmail.com>
2024-11-08 12:26:09 +0530

    added Vercel.json for hosting

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-const express = require('express');
-const app = express()
-const testroute = require('./routers/Test.router');
-const userroute =require('./routers/user.routes');
-const adminRouter = require('./routers/admin.router');
-const practicetestRouter= require('./routers/practicetest.router')
-require('dotenv').config();
-
-
+const express = require("express");
+const app = express();
+const testroute = require("./routers/Test.router");
+const userroute = require("./routers/user.routes");
+const adminRouter = require("./routers/admin.router");
+const practicetestRouter = require("./routers/practicetest.router");
+require("dotenv").config();
 
 const bodyparser = require("body-parser");
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-
-const path = require('path');
+const path = require("path");
 
 // Serve static files from the 'public' directory
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
-
-app.use('/api/tests', testroute);
-app.use('/api', userroute);
-app.use('/admin', adminRouter);
-app.use('/practicetest', practicetestRouter);
+app.use("/api/tests", testroute);
+app.use("/api", userroute);
+app.use("/admin", adminRouter);
+app.use("/practicetest", practicetestRouter);
 
 const notificationRoutes = require("./routers/Notification.router");
 app.use("/notifications", notificationRoutes);
- 
+
 const courseRoute = require("./routers/course.router");
 app.use("/course", courseRoute);
 
-const reviewRoute= require("./routers/review.router");
+const reviewRoute = require("./routers/review.router");
 app.use("/review", reviewRoute);
 
 const globalSearch = require("./routers/serach.router");
 app.use("/search", globalSearch);
+app.get("/", (req, res) => {
+  res.send("welcome :-)");
+});
 
 module.exports = app;
